87ebc5575e666d433dd1dbedbe8a84900582fada
mike <programmermike1@gmail.com>
2025-09-28 23:46:30 -0700

    fixup rebase conflicts

--- keyboards/boardsource/lulu/keymaps/micleo2/keymap.c
+++ keyboards/boardsource/lulu/keymaps/micleo2/keymap.c
@@ -1,8 +1,11 @@
 // Copyright 2022 Cole Smith <cole@boadsource.xyz>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "caps_word.h"
 #include "color.h"
 #include "gw_oled.h"
+#include "os_detection.h"
+#include "process_combo.h"
 #include "quantum.h"
 #include "rgb_matrix.h"
 #include "steno_keycodes.h"
@@ -63,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,        KC_1,          KC_2,         KC_3,          KC_4,          KC_5,                                        KC_6,          KC_7,          KC_8,         KC_9,          KC_0,          QK_BOOT,
   TG(_PLV),      KC_Q,          KC_W,         KC_E,          KC_R,          KC_T,                                        KC_Y,          KC_U,          KC_I,         KC_O,          KC_P,          KC_DEL,
   PWR_SFT,       KC_A,          BSE_S,        BSE_D,         BSE_F,         KC_G,                                        KC_H,          KC_J,          KC_K,         KC_L,          KC_SCLN,       OSL(Y),
-  TG(_BLN),      KC_Z,          KC_X,         KC_C,          KC_V,          KC_B,          RGB_VAI,       RGB_VAD,       KC_N,          KC_M,          KC_COMM,      KC_DOT,        KC_COLN,       C(G(KC_Q)),
+  TG(_BLN),      KC_Z,          KC_X,         KC_C,          KC_V,          KC_B,          _______,       _______,       KC_N,          KC_M,          KC_COMM,      KC_DOT,        KC_COLN,       C(G(KC_Q)),
                                 KC_TAB,       CTL_T(KC_ESC), BSE_LTB,       SFT_T(KC_ENT),                               HYPR_T(KC_SPC),SFT_T(KC_BSPC),OSL(M),       TG(_GME)
 ),
 
